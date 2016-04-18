9adeb93d9a3148f0a54f879b710553878d24bf74
Akash <acsant@uwaterloo.ca>
2016-04-18 03:21:45 -0400

    operator test6

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ var app = express()
   var token = "CAACZClHELlZCIBAJZB9gT66Ca9TP6h4BuOWdp4frFeI4sQMJIqqXMNoeMLli2GNXO5GQGKRcFzZB3G9132hHdJgpKLWgX8JjR9O2kcauuHaXd5ncJBKhEtLEnsBOSxl2usYoIyFiRgazKEx3hXEQbfvN7bcSkAea2jycN9RNuX3lLSU96pXqO2X8ZAT2g4mAZD"
   var apikey = "43073ca8eccde3bb5744d4df7cc9dec3"
   var openapi = require('uwapi')(apikey)
-  var matchGT = /(?:^|\s)(greater)(?=\s|)/
-  var matchLT = /(?:^|\s)(less)(?=\s|)/
-  var matchEQ = /(?:^|\s)(equal)(?=\s|)/
 
   //Index Route
   app.get('/', function (req, res) {
@@ -42,19 +39,9 @@ var app = express()
       if (event.message && event.message.text) {
         text = event.message.text
         var match = text.match(/\d+/)
-        var GT = text.match(matchGT)
-        var LT = text.match(matchLT)
-        var EQ = text.match(matchEQ)
-        console.log(GT)
         if (match) {
-          sendTextMessage(sender, "AkashBot suggests: ");
-          if (EQ) {
-            findFoodWithCaloriesLessThan(match, sender, EQ);
-          } else if (GT) {
-            findFoodWithCaloriesLessThan(match, sender, GT);
-          } else {
-            findFoodWithCaloriesLessThan(match, sender, LT);
-          }
+          sendTextMessage(sender, "AkashBot suggests: ")
+          findFoodWithCaloriesLessThan(match, sender)
           //sendTextMessage(sender, "Text received, echo: " + match)
         }
       }
@@ -63,17 +50,8 @@ var app = express()
   })
 
   // calories endpiint
-  function findFoodWithCaloriesLessThan (calories, sender, oper) {
-    var param = ''
-    if (oper[0] == 'equal') {
-      param = {'calories.eq': calories}
-    } else if (oper[0] == 'greater') {
-      console.log('reached')
-      param = {'calories.gt': calories}
-    } else {
-      param = {'calories.lt': calories}
-    }
-    openapi.foodservicesSearch({}, param).then(function(foods) {
+  function findFoodWithCaloriesLessThan (calories, sender) {
+    openapi.foodservicesSearch({}, {'calories.lt': calories}).then(function(foods) {
       var len = foods.length
       if (len > 10)
         len = 10
@@ -82,7 +60,7 @@ var app = express()
                       + "\nCalories: " + JSON.stringify(foods[i].calories)
                       + "\nType: " + JSON.stringify(foods[i].diet_type);
         console.log('Found results for: ', foodInfo)
-        sendTextMessage(sender, foodInfo);
+        sendTextMessage(sender, foodInfo)
       }
     }, function (err) {
       console.log('Error: ', err)
