b07822ee40a438abf9407de65428430c99f91669
Akash <acsant@uwaterloo.ca>
2016-04-18 00:13:12 -0400

    testing only

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ var app = express()
       sender = event.sender.id
       if (event.message && event.message.text) {
         text = text.message.text
+        if (text == 'Test Generic') {
+          sendGenericMessage(sender)
+          continue
+        }
         sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
       }
     }
@@ -60,5 +64,45 @@ var app = express()
         } else if (response.body.error) {
           console.log('Error', response.body.error)
         }
+    })
+  }
+
+  function sendGenericMessage(sender) {
+    messageData = {
+      "attachment": {
+        "type":"template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "First Card",
+            "subtitle": "Element #1 of an hscrol",
+            "image_url":"http://www.macmillandictionaryblog.com/wp-content/uploads/2011/07/Small-Talk-image.jpg",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.messenger.com",
+              "title": "web url"
+            }, {
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for first element in a generic bubble",
+            }],
+          }]
+        }
+      }
+    }
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: messageData,
+      }, function (error, response, body) {
+        if (error) {
+          console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+          console.log('Error: ', response.body.error)
+        }
+      }
     })
   }
