371146e3734d828c32ef659295cb34a8a125fd32
Akash <acsant@uwaterloo.ca>
2016-04-18 02:08:33 -0400

    performance test4

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ var app = express()
     openapi.foodservicesSearch({}, {'calories.lt': calories}).then(function(foods) {
       var len = foods.length
       for (var i = 0; i < len; i++) {
-        //var foodInfo = "\nFood: " + foods[i].product_name
-        //              + "\nCalories: " + foods[i].calories + 
-        //              + "\nType: " + food[i].diet_type
-        console.log('Found results for: ', foods[i].product_name)
+        var foodInfo = "\nFood: " + JSON.stringify(foods[i].product_name)
+                      + "\nCalories: " + JSON.stringify(foods[i].calories) + 
+                      + "\nType: " + JSON.stringify(food[i].diet_type)
+        console.log('Found results for: ', foodsInfo)
         //sendTextMessage(sender, "AkashBot suggests: " + foodInfo)
       }
     }, function (err) {
