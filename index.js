68d202567377d33fc22ba10981f6df448fe302f8
Akash <acsant@uwaterloo.ca>
2016-04-18 03:14:47 -0400

    operator test

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var app = express()
   var token = "CAACZClHELlZCIBAJZB9gT66Ca9TP6h4BuOWdp4frFeI4sQMJIqqXMNoeMLli2GNXO5GQGKRcFzZB3G9132hHdJgpKLWgX8JjR9O2kcauuHaXd5ncJBKhEtLEnsBOSxl2usYoIyFiRgazKEx3hXEQbfvN7bcSkAea2jycN9RNuX3lLSU96pXqO2X8ZAT2g4mAZD"
   var apikey = "43073ca8eccde3bb5744d4df7cc9dec3"
   var openapi = require('uwapi')(apikey)
-  var matchGT = /(?:^|\s)(greater|more)(?=\s|)/
-  var matchLT = /(?:^|\s)(less|smaller)(?=\s|)/
-  var matchEQ = /(?:^|\s)(equal|same)(?=\s|)/
+  var matchGT = /(?:^|\s)(greater)(?=\s|)/
+  var matchLT = /(?:^|\s)(less)(?=\s|)/
+  var matchEQ = /(?:^|\s)(equal)(?=\s|)/
 
   //Index Route
   app.get('/', function (req, res) {
@@ -47,13 +47,13 @@ var app = express()
         var EQ = text.match(matchEQ)
         console.log(GT)
         if (match) {
-          sendTextMessage(sender, "AkashBot suggests: ")
+          sendTextMessage(sender, "AkashBot suggests: ");
           if (EQ) {
-            findFoodWithCaloriesLessThan(match, sender, EQ)
+            findFoodWithCaloriesLessThan(match, sender, EQ);
           } else if (GT) {
-            findFoodWithCaloriesLessThan(match, sender, GT)
+            findFoodWithCaloriesLessThan(match, sender, GT);
           } else {
-            findFoodWithCaloriesLessThan(match, sender, LT)
+            findFoodWithCaloriesLessThan(match, sender, LT);
           }
           //sendTextMessage(sender, "Text received, echo: " + match)
         }
@@ -82,7 +82,7 @@ var app = express()
                       + "\nCalories: " + JSON.stringify(foods[i].calories)
                       + "\nType: " + JSON.stringify(foods[i].diet_type);
         console.log('Found results for: ', foodInfo)
-        sendTextMessage(sender, foodInfo)
+        sendTextMessage(sender, foodInfo);
       }
     }, function (err) {
       console.log('Error: ', err)
